e886b7df69e3599f579c829426bab883994227c6
Sanyam <64831699+sanyam01@users.noreply.github.com>
2020-10-30 22:06:12 +0530

    Formatted file

--- Exercise1/Server.java
+++ Exercise1/Server.java
@@ -10,19 +10,20 @@ import java.net.Socket;
  * Server class for implementing the server side of the code.
  * 
  * It waits for the user input and runs until user inputs 'QUIT'.
- */ 
+ */
 public class Server {
-	
+
 	/**
 	 * aSocket is of type Socket that actually facilitates the communication
 	 */
 	private Socket aSocket;
 	/**
-	 * serverSocket creates a ServerSocket object, indicating a port number that is not used by other servers
+	 * serverSocket creates a ServerSocket object, indicating a port number that is
+	 * not used by other servers
 	 */
 	private ServerSocket serverSocket;
 	/**
-	 * socketOut is of type PrintWriter for providing the output to the socket 
+	 * socketOut is of type PrintWriter for providing the output to the socket
 	 */
 	private PrintWriter socketOut;
 	/**
@@ -31,7 +32,8 @@ public class Server {
 	private BufferedReader socketIn;
 
 	/**
-	 * Constructs a constructor of class Server, which assigns the port number to the serverSocket.
+	 * Constructs a constructor of class Server, which assigns the port number to
+	 * the serverSocket.
 	 */
 	public Server() {
 		try {
@@ -41,13 +43,12 @@ public class Server {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
+
 	/**
-	 * isPalindrome() reads the output from the socket, and checks if the read number is Palindrome or not.
+	 * isPalindrome() reads the output from the socket, and checks if the read
+	 * number is Palindrome or not.
 	 * 
-	 * After checking it writes the output to the socket. 
+	 * After checking it writes the output to the socket.
 	 */
 	public void isPalindrome() {
 		String line = null;
@@ -57,30 +58,30 @@ public class Server {
 				if (line == null) {
 					break;
 				}
-				// Pointers pointing to the beginning 
-		        // and the end of the string 
-		        int i = 0, j = line.length() - 1;
-		        boolean flag = true;
-		  
-		        // While there are characters to compare 
-		        while (i < j) { 
-		  
-		            // If there is a mismatch 
-		            if (line.charAt(i) != line.charAt(j)) {
-		            	socketOut.println(line + " is not palindrome"); 
-		            	flag = false;
-		            	break;
-		            }
-		  
-		            // Increment first pointer and 
-		            // decrement the other 
-		            i++; 
-		            j--; 
-		        } 
-		  
-		        // Given string is a palindrome 
-		        if (flag)
-		        	socketOut.println(line + " is palindrome");
+				// Pointers pointing to the beginning
+				// and the end of the string
+				int i = 0, j = line.length() - 1;
+				boolean flag = true;
+
+				// While there are characters to compare
+				while (i < j) {
+
+					// If there is a mismatch
+					if (line.charAt(i) != line.charAt(j)) {
+						socketOut.println(line + " is not palindrome");
+						flag = false;
+						break;
+					}
+
+					// Increment first pointer and
+					// decrement the other
+					i++;
+					j--;
+				}
+
+				// Given string is a palindrome
+				if (flag)
+					socketOut.println(line + " is palindrome");
 			} catch (IOException e) {
 				e.printStackTrace();
 			} // reading from the client
@@ -91,7 +92,7 @@ public class Server {
 	public static void main(String[] args) throws IOException {
 
 		Server myServer = new Server();
-		//establishing connection
+		// establishing connection
 		try {
 			myServer.aSocket = myServer.serverSocket.accept();
 			System.out.println("Server is now running");
@@ -99,15 +100,15 @@ public class Server {
 			myServer.socketIn = new BufferedReader(new InputStreamReader(myServer.aSocket.getInputStream()));
 			myServer.socketOut = new PrintWriter(myServer.aSocket.getOutputStream(), true);
 			myServer.isPalindrome();
-			
+
 			myServer.socketIn.close();
 			myServer.socketOut.close();
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
